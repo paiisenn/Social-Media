d1031d68a0f52103b7b2ad958998c17bd4891c93
Hoa Pham <hoaphamxuan2002@gmail.com>
2026-04-20 03:45:26 +0700

    Add selenium github action for login

--- my-app/tests/auth/login.test.js
+++ my-app/tests/auth/login.test.js
@@ -4,18 +4,18 @@ const { Builder, By, until } = require("selenium-webdriver");
     let driver = await new Builder().forBrowser("chrome").build();
 
     try {
-        await driver.get("http://localhost:5000/login");
+        await driver.get("https://social-media-frontend-94uz.onrender.com/login");
 
         await driver.wait(until.elementLocated(By.id("email")), 10000);
 
-        await driver.findElement(By.id("email")).sendKeys("test@gmail.com");
-        await driver.findElement(By.id("password")).sendKeys("123456");
+        await driver.findElement(By.id("email")).sendKeys("phamxuanhoa@gmail.com");
+        await driver.findElement(By.id("password")).sendKeys("123456789");
 
         await driver.findElement(By.css('button[type="submit"]')).click();
 
         // thử chờ chuyển trang
         try {
-            await driver.wait(until.urlIs("http://localhost:5000/"), 7000);
+            await driver.wait(until.urlIs("https://social-media-frontend-94uz.onrender.com/"), 7000);
             console.log(" LOGIN PASS");
         } catch {
             console.log(" Không vào /home");
