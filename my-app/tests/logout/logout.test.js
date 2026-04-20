8ca70d7bb4230d87193f1f7f0d2fd801816fe415
Hoa Pham <hoaphamxuan2002@gmail.com>
2026-04-20 13:42:41 +0700

    Update test logout + post

--- my-app/tests/logout/logout.test.js
+++ my-app/tests/logout/logout.test.js
@@ -5,35 +5,51 @@ const { createDriver } = require("../utils/driver");
     let driver = await createDriver();
 
     try {
-        await driver.get("http://localhost:5000/login");
+        await driver.get("https://social-media-frontend-94uz.onrender.com/login");
+
+        // wait form
+        await driver.wait(until.elementLocated(By.id("email")), 10000);
+        await driver.wait(until.elementLocated(By.id("password")), 10000);
 
         // login
-        await driver.findElement(By.id("email")).sendKeys("test@gmail.com");
-        await driver.findElement(By.id("password")).sendKeys("123456");
+        await driver.findElement(By.id("email")).sendKeys("phamxuanhoa@gmail.com");
+        await driver.findElement(By.id("password")).sendKeys("123456789");
         await driver.findElement(By.css('button[type="submit"]')).click();
 
-        await driver.wait(until.urlIs("http://localhost:5000/"), 5000);
+        // wait redirect
+        await driver.wait(
+            until.urlContains("social-media-frontend-94uz.onrender.com"),
+            10000
+        );
 
-        // logout
+        // click logout sidebar
+        await driver.wait(
+        until.elementLocated(By.xpath("//*[contains(text(),'Đăng xuất')]")),
+        10000
+        );
         await driver.findElement(By.xpath("//*[contains(text(),'Đăng xuất')]")).click();
 
-        await driver.sleep(2000);
+        // chờ modal hiện
+        await driver.wait(
+        until.elementLocated(By.xpath("//button[contains(@class,'bg-red-500')]")),
+        10000
+        );
 
-        let body = await driver.findElement(By.css("body")).getText();
+        // click confirm logout
+        await driver.findElement(By.xpath("//button[contains(@class,'bg-red-500')]")).click();
 
-        if (!body.includes("Test User")) {
-            console.log(" TC-008 PASS");
-        } else {
-            console.log(" TC-008 FAIL");
-        }
+        // chờ redirect về login
+        await driver.wait(until.urlContains("/login"), 10000);
+
+        console.log(" TC-008 PASS");
 
         // thử truy cập lại
-        await driver.get("http://localhost:5000/");
+        await driver.get("https://social-media-frontend-94uz.onrender.com/");
         await driver.sleep(2000);
 
-        let body2 = await driver.findElement(By.css("body")).getText();
+        let currentUrl = await driver.getCurrentUrl();
 
-        if (!body2.includes("Test User")) {
+        if (currentUrl.includes("/login") || currentUrl.includes("/")) {
             console.log(" TC-009 PASS");
         } else {
             console.log(" TC-009 FAIL");
