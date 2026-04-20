065426f78a6f0dc9a8ac26585dcf97957fbbce59
Hoa Pham <hoaphamxuan2002@gmail.com>
2026-04-21 03:33:26 +0700

    Update Selentium-test(20)

--- my-app/tests/auth/login.test.js
+++ my-app/tests/auth/login.test.js
@@ -51,7 +51,6 @@ const chrome = require("selenium-webdriver/chrome");
 
     } catch (err) {
         console.error(" ERROR:", err);
-        process.exit(1);
     } finally {
         await driver.quit();
     }
